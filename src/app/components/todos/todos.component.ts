abe17638d9ca240324e2acc39e547a04dfb5ee5e
bbapm30 <stolarczyk.dominik@gmail.com>
2019-02-02 20:12:32 +0100

    Json Placholder Todos Added

--- src/app/components/todos/todos.component.ts
+++ src/app/components/todos/todos.component.ts
@@ -1,22 +1,25 @@
-import { Todo } from './../../models/Todo';
- import { Component, OnInit } from '@angular/core';
+import { TodoService } from "./../../services/todo.service";
+import { Todo } from "./../../models/Todo";
+import { Component, OnInit } from "@angular/core";
 
 @Component({
-  selector: 'app-todos',
-  templateUrl: './todos.component.html',
-  styleUrls: ['./todos.component.css']
+  selector: "app-todos",
+  templateUrl: "./todos.component.html",
+  styleUrls: ["./todos.component.css"]
 })
 export class TodosComponent implements OnInit {
-  todos:Todo[];
+  todos: Todo[];
 
-  constructor() { }
+  constructor(private todoService: TodoService) {}
 
   ngOnInit() {
-    this.todos = [
-      {id:1, title: 'Todo One', completed: false},
-      {id:2, title: 'Todo Two', completed: true},
-      {id:3, title: 'Todo Three', completed: false},
-    ]
+    this.todoService.getTodos().subscribe(todos => {
+      this.todos = todos;
+    });
   }
 
+  deleteTodo(todo: Todo) {
+    this.todos = this.todos.filter(t => t.id !== todo.id);
+    this.todoService.deleteTodo(todo).subscribe();
+  }
 }
